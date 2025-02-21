1eb0e1e77a08bcf2b68e6e19869eba5978bf3fd5
Christian <christian.eieland@hotmail.com>
2025-02-21 13:07:06 +0100

    dwe

--- ConsoleApp1/cheatsheet.cs
+++ ConsoleApp1/cheatsheet.cs
@@ -3,3 +3,4 @@
 // /* Console.WriteLine = Line er som å trykke på Enterknapp etter endt linje, 
 // // mens kun Write i selv flere kodelinjer ville fortsatt på samme outputlinje
 
+// bruke Console.Readline når man skal ha input fra terminal ved kjøring av programmet fremfor å ha ferdig input i kode
